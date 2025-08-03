ef5bcc97fe835a4976ce5b84bc1cc8a7b3249fb8
Shreya Shristii <shreyashristii@gmail.com>
2025-08-03 23:39:24 +0530

    Sunday Funday

---
+++ JSDay1.js
@@ -0,0 +1,32 @@
+console.log("hiiiii")
+function sayhi(){
+    alert("hellooo");
+}
+const product={
+    title:"Ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+}
+const profile={
+    username:"Shreya",
+    isFollow:true,
+    followers:200,
+    following:230,
+
+}
+let a=5, b=10;
+if(a<b){
+    console.log("a is the smaller one");
+}
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+alert("This is a corrupted website");
+let n=prompt("Enter a number to check");
+if(n%5==0){
+    console.log("Yes a multiple");
+}
+else{
+    console.log("No");
+}
